98120fef6a1058f0bd6a8e678139c648dccfbb96
Vinicius Duarte <viniciusduarte@outlook.com>
2021-02-23 09:20:36 -0300

    add post route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) => {
+  return response.json({message: "Hello World!"});
+});
 
+app.post('/', (request, response) => {
+  return response.json({message: "Done"})
+})
 
 app.listen(3333, () => console.log("Server is running! "));
