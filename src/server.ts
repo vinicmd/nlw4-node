59f07c680ac8dc4e8eb9ee1da42e32c0489a4a14
Vinicius Duarte <viniciusduarte@outlook.com>
2021-02-25 12:56:46 -0300

    Config database, controllers, models of users

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: "Hello World!"});
-});
+app.use(express.json());
 
-app.post('/', (request, response) => {
-  return response.json({message: "Done"});
-})
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running! "));
